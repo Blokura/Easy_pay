c073c4bc16b545985178c969d98a25690c917a01
Blokura <2913434936@qq.com>
2018-11-11 11:16:55 +0800

    http改为https

--- SDK/epay.config.php
+++ SDK/epay.config.php
@@ -21,8 +21,8 @@ $alipay_config['sign_type']    = strtoupper('MD5');
 $alipay_config['input_charset']= strtolower('utf-8');
 
 //访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
-$alipay_config['transport']    = 'http';
+$alipay_config['transport']    = 'https';
 
 //支付API地址
-$alipay_config['apiurl']    = 'http://pay.moebili.com/';
+$alipay_config['apiurl']    = 'https://pay.moebili.com/';
 ?>
